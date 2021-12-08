b4b6d4c43ff26da560d04dd8d7a961370444a862
HidayahHamri <nurhidayahhamri@gmail.com>
2021-12-08 22:11:25 +0800

    Co-authored-by: Hasanah15 <Hasanah15@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -5,27 +5,27 @@
 //? Exercise 3: Navigation and Routing
 
 // Date: 12 December 2021
-// Member 1's Name: _____________    Section: _____   Location: ____________ (i.e. where are you currently located)
-// Member 2's Name: _____________    Section: _____   Location: ____________
+// Member 1's Name: Nur Hidayah Binti Hamri      Section: 02   Location: Segamat, Johor (i.e. where are you currently located)
+// Member 2's Name: Nur Hasanah Binti Sariddon   Section: 02   Location: Kuala Berang, Hulu Terengganu
 
 // Log the time(s) your pair programming sessions
 //  Date         Time (From)   To             Duration (in minutes)
-//  _________    ___________   ___________    ________
+//  8/12/2021     9:50 p.m      ___________    ________
 //  _________    ___________   ___________    ________
 //  _________    ___________   ___________    ________
 
 import 'package:flutter/material.dart';
 
-// import 'models/mock_todos.dart';
+import 'models/mock_todos.dart';
 import 'screens/todo_list/todo_list_screen.dart';
-// import 'screens/task_list/task_list_screen.dart'; //? Later you want to remove this line
+import 'screens/task_list/task_list_screen.dart'; //? Later you want to remove this line
 
 void main() => runApp(
       MaterialApp(
         title: 'MAP Exercise 3',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(primarySwatch: Colors.purple),
-        home: TodoListScreen(),
+        home: TodoListScreen(myTodoList),
         // home: TaskListScreen(), //? uncomment this out to preview the second screen. Later, you want to remove this line
       ),
     );
--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,5 +1,27 @@
 // TODO Define the model class Todo here. The attributes are given. Complete the rest.
+import 'package:flutter/cupertino.dart';
+import 'task.dart';
 
-// import 'task.dart';
+class Todo {
+  String _title;
+  List<Task> _items;
 
-class Todo {}
+  double get progress {
+    int completed = 0;
+    for (int i = 0; i < _items.length; i++) {
+      if (items[i].completed) {
+        completed = completed + 1;
+      }
+    }
+    return (completed / _items.length) * 100;
+  }
+
+  get items => _items;
+  get title => _title;
+
+  Todo({title, items})
+      : _title = title,
+        _items = items;
+
+  Todo.copy(Todo from) : this(title: from.title, items: [...from.items]);
+}
