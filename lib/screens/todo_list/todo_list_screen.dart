71b6b904628c87c67d719711dd08d93096a810f9
HidayahHamri <nurhidayahhamri@gmail.com>
2021-12-08 23:17:15 +0800

    update todo list screen for todo list

--- lib/screens/todo_list/todo_list_screen.dart
+++ lib/screens/todo_list/todo_list_screen.dart
@@ -1,12 +1,16 @@
 // TODO Complete this file. This class should receive data from the main(). This class should pass data to the classes Bar and Body
 
+import 'package:exercise2/models/todo.dart';
 import 'package:flutter/material.dart';
 
-// import '../../models/todo.dart';
+import '../../models/todo.dart';
 import 'bar.dart';
 import 'body.dart';
 
 class TodoListScreen extends StatefulWidget {
+  TodoListScreen(this._todo);
+  final List<Todo> _todo;
+
   @override
   _TodoListScreenState createState() => _TodoListScreenState();
 }
@@ -19,7 +23,7 @@ class _TodoListScreenState extends State<TodoListScreen> {
       child: SafeArea(
         child: Scaffold(
           appBar: Bar(),
-          body: Body(),
+          body: Body(widget._todo),
         ),
       ),
     );
