e42fa9aedda01ca82469449a97474ee1f85888ef
HidayahHamri <nurhidayahhamri@gmail.com>
2021-12-08 23:48:49 +0800

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,14 @@
 // Member 2's Name: Nur Hasanah Binti Sariddon   Section: 02   Location: Kuala Berang, Hulu Terengganu
 
 // Log the time(s) your pair programming sessions
-//  Date         Time (From)   To             Duration (in minutes)
-//  8/12/2021     9:50 p.m      ___________    ________
-//  _________    ___________   ___________    ________
-//  _________    ___________   ___________    ________
+//  Date         Time (From)       To             Duration (in minutes)
+//  8/12/2021     9:50 p.m.      11:30 p.m.        1 hour 40 minutes
+//  8/12/2021     11:45p.m.      11:50 p.m.             5 minuts
 
 import 'package:flutter/material.dart';
 
 import 'models/mock_todos.dart';
 import 'screens/todo_list/todo_list_screen.dart';
-//import 'screens/task_list/task_list_screen.dart'; //? Later you want to remove this line
 
 void main() => runApp(
       MaterialApp(
@@ -26,6 +24,5 @@ void main() => runApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(primarySwatch: Colors.purple),
         home: TodoListScreen(myTodoList),
-        // home: TaskListScreen(), //? uncomment this out to preview the second screen. Later, you want to remove this line
       ),
     );
