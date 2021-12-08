0111ae65d3177f1f63dc421490aada609662f65b
HidayahHamri <nurhidayahhamri@gmail.com>
2021-12-08 23:13:32 +0800

    update body under todo list

--- lib/screens/todo_list/body.dart
+++ lib/screens/todo_list/body.dart
@@ -1,26 +1,55 @@
 // TODO Complete this file. This class should receive data from the TodoListScreen. The UI structure has been provided
-//
+
+//import 'package:exercise2/models/todo.dart';
 import 'package:flutter/material.dart';
+import '../../models/todo.dart';
+import '../task_list/task_list_screen.dart';
 
-// import '../../models/todo.dart';
-// import '../task_list/task_list_screen.dart';
+class Body extends StatefulWidget {
+  Body(this._todo);
+  final List<Todo> _todo;
 
-class Body extends StatelessWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  void _navigate({context, index}) async {
+    Todo result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => TaskListScreen(Todo.copy(widget._todo[index])),
+      ),
+    );
+
+    if (result == null) {
+    } else {
+      setState(() => widget._todo[index] = result);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
-      itemCount: 4,
-      separatorBuilder: (context, index) => Divider(
-        color: Colors.blueGrey,
-      ),
-      itemBuilder: (context, index) => ListTile(
-        title: Text('Todo title'),
-        subtitle: Text('Sub title'),
-        trailing: CircleAvatar(
-          child: Text('100'),
-        ),
-        onTap: () {},
-      ),
-    );
+        itemCount: widget._todo.length,
+        separatorBuilder: (context, index) => Divider(
+              color: Colors.blueGrey,
+            ),
+        itemBuilder: (context, index) => ListTile(
+              title: Text(widget._todo[index].title),
+              subtitle: Text('This list has' +
+                  widget._todo[index].items.length.toString() +
+                  "task(s)"),
+              trailing: CircleAvatar(
+                child: Text(
+                  widget._todo[index].progress.round().toString(),
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              onTap: () => _navigate(
+                context: context,
+                index: index,
+              ),
+            ));
   }
 }
